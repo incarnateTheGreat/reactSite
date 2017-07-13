f1359abd31c1fab725ca1ef8fb2273716ff1ebbc
Garry Tsaconas <garrytsaconas@insightactioninc.com>
2017-07-13 02:49:21 -0400

    Disabled Redux-Logger console output.

--- src/js/store.js
+++ src/js/store.js
@@ -6,6 +6,8 @@ import promise from 'redux-promise-middleware'
 //Redux Reducers
 import reducer from './reducers/index'
 
-const middleware = applyMiddleware(promise(), thunk, createLogger())
+const middleware = applyMiddleware(promise(), thunk, createLogger({
+  predicate: () => false //Enable to display Redux-Logger output in the console.
+}))
 
 export default createStore(reducer, middleware)
